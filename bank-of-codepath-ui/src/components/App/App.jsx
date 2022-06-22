87dfd33632182c4b119a077018d3fe76ef232062
milesba4 <milesbaffour@gmail.com>
2022-06-22 11:34:54 -0400

    imporitng Transactional detail in app.jsx component

--- bank-of-codepath-ui/src/components/App/App.jsx
+++ bank-of-codepath-ui/src/components/App/App.jsx
@@ -2,6 +2,7 @@ import * as React from "react"
 import Navbar from "../Navbar/Navbar"
 import Home from "../Home/Home"
 import "./App.css"
+import TransactionDetail from "../TransactionDetail/TransactionDetail"
 
 export default function App() {
   return (
