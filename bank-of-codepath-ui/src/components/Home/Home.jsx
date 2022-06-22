597020fb208875274e3f43f93423bdf060052781
milesba4 <milesbaffour@gmail.com>
2022-06-22 16:46:58 -0400

    creating useEffect functions

--- bank-of-codepath-ui/src/components/Home/Home.jsx
+++ bank-of-codepath-ui/src/components/Home/Home.jsx
@@ -7,6 +7,38 @@ import axios from "axios"
 
 
 export default function Home({transactions,setTransactions,transfers,setTransfers,error,setError,isLoading,setIsLoading,filterInputValue}) {
+   useEffect(async() => {
+    setIsLoading(true);
+    try{
+      const response = await axios.get('http://localhost:3001/bank/transactions')
+      setTransactions(response)
+      console.log("response1=", response)
+    }
+    catch (error){
+      setError(error)
+      
+      console.log("error1=", error)
+    }
+    setIsLoading(false);
+  }, []);
+
+  useEffect(async() => {
+    setIsLoading(true);
+    try{
+      const response = await axios.get('http://localhost:3001/bank/transfers')
+      setTransfers(response.data.transfers)
+      console.log("response2=", response)
+      
+    }
+    catch (error){
+      setError(error)
+      console.log("error2=", error)
+    }
+    setIsLoading(true);
+  
+  }, []);
+
+  console.log(transactions) // checking transactions
     return transactions
   }
 
