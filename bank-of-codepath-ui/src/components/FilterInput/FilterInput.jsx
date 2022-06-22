edb0064a6bd1a6b15d93b2d37490154ac76a66ea
milesba4 <milesbaffour@gmail.com>
2022-06-22 13:40:50 -0400

    Update FilterInput.jsx

--- bank-of-codepath-ui/src/components/FilterInput/FilterInput.jsx
+++ bank-of-codepath-ui/src/components/FilterInput/FilterInput.jsx
@@ -1,11 +1,11 @@
 import * as React from "react"
 import "./FilterInput.css"
 
-export default function FilterInput() {
+export default function FilterInput({handleOnChange, inputValue}) {
   return (
     <div className="filter-input">
       <i className="material-icons">search</i>
-      <input type="text" placeholder="Search transactions" />
+      <input value = {inputValue} onChange= {handleOnChange} type="text" placeholder="Search transactions" />
     </div>
   )
 }
