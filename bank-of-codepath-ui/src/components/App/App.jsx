db6920bff22f483be8b6d172911ce8a386642c6c
milesba4 <milesbaffour@gmail.com>
2022-06-22 11:35:13 -0400

    importing browserRouter, Routes, and Route in app.jsx component

--- bank-of-codepath-ui/src/components/App/App.jsx
+++ bank-of-codepath-ui/src/components/App/App.jsx
@@ -2,6 +2,7 @@ import * as React from "react"
 import Navbar from "../Navbar/Navbar"
 import Home from "../Home/Home"
 import "./App.css"
+import {BrowserRouter,Routes,Route} from "react-router-dom"
 import TransactionDetail from "../TransactionDetail/TransactionDetail"
 
 export default function App() {
