1ac99743cfdaf4cddc7e2228a34db55f3fe2ea62
milesba4 <milesbaffour@gmail.com>
2022-06-22 16:46:04 -0400

    uncommenting line on main.jsx

--- bank-of-codepath-ui/src/components/Home/Home.jsx
+++ bank-of-codepath-ui/src/components/Home/Home.jsx
@@ -2,12 +2,25 @@ import * as React from "react"
 import AddTransaction from "../AddTransaction/AddTransaction"
 import BankActivity from "../BankActivity/BankActivity"
 import "./Home.css"
+import { useEffect } from 'react';
+import axios from "axios"
+
+
+export default function Home({transactions,setTransactions,transfers,setTransfers,error,setError,isLoading,setIsLoading,filterInputValue}) {
+    return transactions
+  }
+
 
 export default function Home() {
   return (
     <div className="home">
       <AddTransaction />
       <BankActivity />
+      <AddTransaction/>
+
     </div>
+
+  
   )
+
 }
